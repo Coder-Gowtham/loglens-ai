d23fe0fdd925741831c9fcded77f47d7e1a8e7b7
Gowtham <codewithgowtham.kv@gmail.com>
2026-05-15 18:57:05 +0530

    removed console logs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import app from "./app.js";
 import { env } from "./config/env.js";
 
+console.log("Trust proxy from server.js:", app.get("trust proxy"));
+
+
 app.listen(env.port, () => {
   console.log(`LogLens AI backend running on http://localhost:${env.port}`);
 });
